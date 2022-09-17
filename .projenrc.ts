b2b5dd6dfcbcf65989b046e82218b1ed34c41245
devops-at-home <76842834+devops-at-home@users.noreply.github.com>
2022-09-17 19:59:19 +0800

    update license

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,10 @@
 import { web } from 'projen';
 
-const name = 'devops-at-ho.me-blog';
-
-const commonProps = {
+const project = new web.NextJsTypeScriptProject({
     authorName: 'DevOps@Home',
     authorUrl: 'https://devops-at-ho.me',
     defaultReleaseBranch: 'main',
-    name,
+    name: 'devops-at-ho.me-blog',
     projenrcTs: true,
     gitignore: ['.idea'],
     prettier: true,
@@ -20,10 +18,7 @@ const commonProps = {
     githubOptions: {
         pullRequestLint: false,
     },
-};
-
-const nextJsProject = new web.NextJsTypeScriptProject({
-    ...commonProps,
+    license: 'MIT',
     // deps: [],                /* Runtime dependencies of this module. */
     // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
     // devDeps: [],             /* Build dependencies for this module. */
@@ -31,20 +26,4 @@ const nextJsProject = new web.NextJsTypeScriptProject({
     // tailwind: true,          /* Setup Tailwind CSS as a PostCSS plugin. */
 });
 
-nextJsProject.synth();
-
-// const awsCdkApp = new awscdk.AwsCdkTypeScriptApp({
-//     ...commonProps,
-//     cdkVersion: '2.42.0',
-//     repositoryDirectory: 'infrastructure',
-//     dire
-//     // devDeps: ['esbuild', '@types/aws-lambda', 'aws-lambda', 'aws-sdk', 'aws-cloudformation-custom-resource'],
-//     // autoApproveUpgrades: true,
-//     // autoApproveOptions: {
-//     //   allowedUsernames: ['devops-at-home'],
-//     // },
-// });
-
-// awsCdkApp.jest!.addTestMatch('**/?(*.)@(spec|test).[tj]s?(x)');
-
-// awsCdkApp.synth();
+project.synth();
