4a9a08d806dab475c266ef5f7320f7aa2e96dfc6
devops-at-home <76842834+devops-at-home@users.noreply.github.com>
2022-09-17 16:57:10 +0800

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,15 @@
+import { web } from "projen";
+const project = new web.NextJsTypeScriptProject({
+  authorName: "DevOps@Home",
+  authorUrl: "https://devops-at-ho.me",
+  defaultReleaseBranch: "main",
+  name: "devops-at-ho.me-blog",
+  projenrcTs: true,
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+  // tailwind: true,          /* Setup Tailwind CSS as a PostCSS plugin. */
+});
+project.synth();
