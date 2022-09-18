e312c4490b52c7d4489e937a4987c41f945c5431
devops-at-home <76842834+devops-at-home@users.noreply.github.com>
2022-09-18 16:07:33 +0800

    update projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,16 +1,32 @@
-import { awscdk } from "projen";
-const project = new awscdk.AwsCdkTypeScriptApp({
-  authorName: "DevOps@Home",
-  authorUrl: "https://devops-at-ho.me",
-  cdkVersion: "2.42.0",
-  defaultReleaseBranch: "main",
-  eslint: false,
-  name: "devops-at-ho.me-blog",
-  projenrcTs: true,
+import { awscdk } from 'projen';
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+const project = new awscdk.AwsCdkTypeScriptApp({
+    authorName: 'DevOps@Home',
+    authorUrl: 'https://devops-at-ho.me',
+    cdkVersion: '2.42.0',
+    defaultReleaseBranch: 'main',
+    eslint: false,
+    name: 'devops-at-ho.me-blog',
+    projenrcTs: true,
+    gitignore: ['.idea'],
+    prettier: true,
+    prettierOptions: {
+        settings: {
+            printWidth: 120,
+            tabWidth: 4,
+            singleQuote: true,
+        },
+    },
+    githubOptions: {
+        pullRequestLint: false,
+    },
+    license: 'MIT',
+    // deps: [],                /* Runtime dependencies of this module. */
+    // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+    devDeps: ['@sls-next/cdk-construct', '@sls-next/lambda-at-edge'] /* Build dependencies for this module. */,
+    // packageName: undefined,  /* The "name" in package.json. */
 });
+
+project.jest!.addTestMatch('**/?(*.)@(spec|test).[tj]s?(x)');
+
 project.synth();
