38a069dc9240fd7827537c30f99af83890ac1b43
devops-at-home <76842834+devops-at-home@users.noreply.github.com>
2022-09-27 23:40:24 +0800

    need to increase test timeout

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,22 @@ import { App, Environment } from 'aws-cdk-lib';
 import { Builder } from '@sls-next/lambda-at-edge';
 import { StackFactory } from './stacks/stack-factory';
 
-export const setupBuild = async (app: App, env?: Environment) => {
+export const setupBuild = async (env?: Environment) => {
     const builder = new Builder('app', './build', { args: ['build'], cwd: 'app' });
     await builder.build();
     try {
+        const app = new App();
         new StackFactory(app, {
             env,
         });
+        app.synth();
     } catch (e) {
         console.log(e);
         process.exit(1);
     }
-    const { stacks } = app.synth();
-    return stacks;
 };
 
-const app = new App();
-
-setupBuild(app, {
+setupBuild({
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
 });
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,11 +1,29 @@
-// import { App } from 'aws-cdk-lib';
-// import { setupBuild } from '../src/main';
+import { App } from 'aws-cdk-lib';
+import { Builder } from '@sls-next/lambda-at-edge';
+import { StackFactory } from '../src/stacks/stack-factory';
 
-test('Integration test snapshots', async () => {
-    // This times out because it takes longer than 5000ms
-    // const app = new App();
-    // const stacks = await setupBuild(app);
-    // stacks.forEach((stack) => expect(stack.template).toMatchSnapshot());
+beforeAll(async () => {
+    const builder = new Builder('app', './build', { args: ['build'], cwd: 'app' });
+    await builder.build();
+});
 
-    expect(true).toBeTruthy;
+// test('Integration test snapshots', async () => {
+//     // This times out because it takes longer than 5000ms
+//     // const app = new App();
+//     // const stacks = await setupBuild(app);
+//     // stacks.forEach((stack) => expect(stack.template).toMatchSnapshot());
+
+//     expect(true).toBeTruthy;
+// });
+
+describe('Integration test snapshots', () => {
+    const app = new App();
+
+    new StackFactory(app);
+
+    const { stacks } = app.synth();
+
+    test.each(stacks)(`$stackName`, (stack) => {
+        expect(stack.template).toMatchSnapshot();
+    });
 });
