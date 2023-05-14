b21c4bdc875da524da0a517a51ae644b44f41786
devops-at-home <76842834+devops-at-home@users.noreply.github.com>
2023-05-14 23:24:24 +0800

    chore: project created with projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,40 +1,15 @@
-import { awscdk } from 'projen';
+import { web } from "projen";
+const project = new web.NextJsTypeScriptProject({
+  authorName: "DevOps@Home",
+  authorUrl: "https://devops-at-ho.me",
+  defaultReleaseBranch: "main",
+  name: "devops-at-ho.me-blog",
+  projenrcTs: true,
 
-const project = new awscdk.AwsCdkTypeScriptApp({
-    authorName: 'DevOps@Home',
-    authorUrl: 'https://devops-at-ho.me',
-    cdkVersion: '2.43.1',
-    defaultReleaseBranch: 'main',
-    eslint: false,
-    name: 'devops-at-ho.me-blog',
-    projenrcTs: true,
-    gitignore: ['.idea', 'build'],
-    prettier: true,
-    prettierOptions: {
-        settings: {
-            printWidth: 120,
-            tabWidth: 4,
-            singleQuote: true,
-        },
-    },
-    githubOptions: {
-        pullRequestLint: false,
-    },
-    autoApproveUpgrades: true,
-    autoApproveOptions: {
-        allowedUsernames: ['devops-at-home'],
-    },
-    license: 'MIT',
-    // deps: [],                /* Runtime dependencies of this module. */
-    // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-    devDeps: ['@sls-next/cdk-construct', '@sls-next/lambda-at-edge'] /* Build dependencies for this module. */,
-    // packageName: undefined,  /* The "name" in package.json. */
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+  // tailwind: true,          /* Setup Tailwind CSS as a PostCSS plugin. */
 });
-
-project.jest!.addTestMatch('**/?(*.)@(spec|test).[tj]s?(x)');
-
-project.preCompileTask.prependExec('yarn install', {
-    cwd: 'app',
-});
-
 project.synth();
