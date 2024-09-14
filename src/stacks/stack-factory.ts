84dd69d7a725acc29368a4a3ab0004e6b3051413
devops-at-home <76842834+devops-at-home@users.noreply.github.com>
2024-09-14 16:18:28 +0800

    delete all files

--- src/stacks/stack-factory.ts
+++
@@ -1,8 +0,0 @@
-import { App, StackProps } from 'aws-cdk-lib';
-import { NextjsLambdaStack } from './nextjs-lambda.stack';
-
-export class StackFactory {
-    constructor(app: App, stackProps?: StackProps) {
-        new NextjsLambdaStack(app, `NextjsLambdaStack`, stackProps);
-    }
-}
